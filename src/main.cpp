0b43e06b63ac56ec02642ba9a239b8064dc73b6b
paul.arbez <paul.arbez@epitech.eu>
2026-09-08 16:38:34 +0200

    Add comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include <SFML/Graphics.hpp>
+#include <GL/glew.h>
+#include <iostream>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({800, 600}), "Shimera - SFML3");
+
+    if (!window.setActive(true)) {
+        std::cerr << "[SFML] failed to activate the window!" << '\n';
+        return -1;
+    }
+
+    // When using Shimera, don't forget to initialize GLEW
+    // if (glewInit() != GLEW_OK) {
+    //     std::cerr << "[GLEW] initialization failed!" << '\n';
+    //     return -1;
+    // }
+
     window.setFramerateLimit(60);
+
+    // Create a Backend
+
+    // Create a Framebuffer
+
+    // Create a new instance of EffectPipeline
 
     const sf::Color background(128, 128, 128);
 
@@ -18,10 +38,27 @@ int main()
                 window.close();
         }
 
-        window.clear(background);
-        window.draw(square);
+        if (!window.setActive(true)) {
+            break;
+        }
+
+        // Create the SFML's Render Texture, it is used to render the scene inside it, and then rendered in 2D on the Quad Post Processing.
+        // auto *sfmlRenderTexture = static_cast<sf::RenderTexture*>(sceneFramebuffer->getNativeRenderTarget());
+
+        // You need to draw the scene between our framebuffer bind and unbind
+
+        window.clear(background); // Replace this with the sfmlRenderTexture clear
+        window.draw(square); // Replace this with the sfmlRenderTexture draw
+
+        // window.setActive(true);
+        // glClear(GL_COLOR_BUFFER_BIT);
+        // Add the EffectPipeline render
+
         window.display();
     }
+
+    // Delete the Framebuffer
+    // Delete the Backend
 
     return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,15 @@
 add_rules("mode.debug", "mode.release")
 
+if is_plat("windows") then
+    set_toolchains("msvc")
+end
+
 set_languages("c++23")
 
 add_requires("sfml 3.x", {configs = {graphics = true, window = true, system = true}})
+add_requires("glew")
 
 target("shimera")
     set_kind("binary")
     add_files("src/*.cpp")
-    add_packages("sfml")
+    add_packages("sfml", "glew")
